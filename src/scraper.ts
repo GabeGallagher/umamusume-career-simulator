30ad4a8724aac26431ed36cd130f3e63a44c9a75
GabeGallagher <zeinfandel@yahoo.com>
2025-10-01 21:51:17 -0500

    refactored scraper to iterate multiple characters and support cards

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,49 +1,68 @@
 import puppeteer, { Browser, Page } from "puppeteer";
 import sqlite3 from "sqlite3";
 
-async function parseCharacterElement(characterElements: NodeListOf<Element>) {
-	const firstElement = characterElements[1] as HTMLAnchorElement;
+async function insertIntoDatabase(
+	dataArray: any[],
+	dbName: string,
+	tableName: string
+) {
+	const db = new sqlite3.Database(`${dbName}.db`, (err) => {
+		if (err) {
+			console.error("Error opening database: ", err);
+		}
+	});
 
-	return {
-		href: firstElement.href,
-		text: firstElement.textContent?.trim() || "",
-		innerHTML: firstElement.innerHTML,
-	};
+	db.run(
+		`CREATE TABLE IF NOT EXISTS ${tableName} (
+        id INTEGER PRIMARY KEY,
+        data JSON
+    )`,
+		(err) => {
+			if (err) {
+				console.error("Error creating table: ", err);
+			}
+		}
+	);
+
+	for (const data of dataArray) {
+		db.run(
+			`INSERT OR REPLACE INTO characters (id, data) VALUES (?, ?)`,
+			[data.itemData.card_id, JSON.stringify(data)],
+			(err) => {
+				if (err) console.error("Error inserting data: ", err);
+			}
+		);
+	}
+
+	db.close((err) => {
+		if (err) {
+			console.error("Error closing database: ", err);
+		} else {
+			console.log("Database closed successfully");
+		}
+	});
 }
 
-async function scrapeGameTora() {
+async function scrapeGameTora(
+	target: string,
+	parentClass: string
+): Promise<any> {
 	const browser: Browser = await puppeteer.launch({
 		headless: false,
 		devtools: true,
 		args: ["--no-sandbox", "--disable-setuid-sandbox"],
 	});
-
+	const url: string = `https://gametora.com/umamusume/${target}`;
 	const page: Page = await browser.newPage();
 
 	try {
-		await page.goto("https://gametora.com/umamusume/characters", {
+		await page.goto(url, {
 			waitUntil: "networkidle2",
 			timeout: 30000,
 		});
 
-		// Test. Delete when get all character list is properly implemented
-		const testCharacter = await page.evaluate(() => {
-			const characterElements = document.querySelectorAll(
-				'a[href*="matikanefukukitaru"]'
-			);
-			const firstElement = characterElements[1] as HTMLAnchorElement;
-
-			return {
-				href: firstElement.href,
-				text: firstElement.textContent?.trim() || "",
-				innerHTML: firstElement.innerHTML,
-			};
-		});
-
-		const characterList = await page.evaluate(() => {
-			const parentElement = document.querySelector(
-				".sc-70f2d7f-0.jaayLK"
-			);
+		const elementList = await page.evaluate((selector) => {
+			const parentElement = document.querySelector(selector);
 			return parentElement
 				? Array.from(parentElement.children).map((child) => {
 						const childElement = child as HTMLAnchorElement;
@@ -54,12 +73,13 @@ async function scrapeGameTora() {
 						};
 				  })
 				: [];
-		});
+		}, parentClass);
 
-		const characterDataArray = [];
-		for (const character of characterList) {
-			if (character.href) {
-				await page.goto(character.href, { waitUntil: "networkidle2" });
+		const elementDataArray = [];
+
+		for (const element of elementList) {
+			if (element.href) {
+				await page.goto(element.href, { waitUntil: "networkidle2" });
 
 				const nextData = await page.evaluate(() => {
 					const scriptElement =
@@ -69,86 +89,61 @@ async function scrapeGameTora() {
 						: null;
 				});
 
-				let characterData;
-
 				if (
 					nextData &&
 					nextData.props &&
 					nextData.props.pageProps &&
 					nextData.props.pageProps.itemData
 				) {
-					characterDataArray.push(nextData.props.pageProps);
+					elementDataArray.push(nextData.props.pageProps);
 				} else {
 					throw new Error(
-						`Character data for ${character.text} not found in __NEXT_DATA__`
+						`Data for ${element.text} not found in __NEXT_DATA__`
 					);
 				}
-			} else {
-				throw new Error(`Invalid character href: ${character.text}`);
 			}
 		}
-
-		// Test. Delete when get all character list is properly implemented
-		await page.goto(testCharacter.href, { waitUntil: "networkidle2" });
-
-		const nextData = await page.evaluate(() => {
-			const scriptElement = document.getElementById("__NEXT_DATA__");
-			return scriptElement
-				? JSON.parse(scriptElement.textContent || "{}")
-				: null;
-		});
-		let characterData;
-		if (
-			nextData &&
-			nextData.props &&
-			nextData.props.pageProps &&
-			nextData.props.pageProps.itemData
-		) {
-			characterData = nextData.props.pageProps;
-		} else {
-			throw new Error("Character data not found in __NEXT_DATA__");
-		}
-		// Test. End test character block
-
-		const db = new sqlite3.Database("characters.db", (err) => {
-			if (err) {
-				console.error("Error opening database: ", err);
-			}
-		});
-
-		db.run(
-			`CREATE TABLE IF NOT EXISTS characters (
-            id INTEGER PRIMARY KEY,
-            data JSON
-        )`,
-			(err) => {
-				if (err) {
-					console.error("Error creating table: ", err);
-				}
-			}
-		);
-
-		db.run(
-			`INSERT OR REPLACE INTO characters (id, data) VALUES (?, ?)`,
-			[characterData.itemData.card_id, JSON.stringify(characterData)],
-			(err) => {
-				if (err) console.error("Error inserting data: ", err);
-			}
-		);
-		db.close((err) => {
-			if (err) {
-				console.error("Error closing database: ", err);
-			} else {
-				console.log("Database closed successfully");
-			}
-		});
+		return elementDataArray;
 	} catch (error) {
-		console.error("Error occurred: ", error);
+		console.error(`Error occurred scraping ${target}: `, error);
 	} finally {
 		await browser.close();
 		console.log("Browser closed");
 	}
 }
 
-// Run the debug function
-scrapeGameTora().catch(console.error);
+async function main() {
+	try {
+		const scrapedCharacterData = await scrapeGameTora(
+			"characters",
+			'.sc-70f2d7f-0.jaayLK'
+		);
+		if (scrapedCharacterData) {
+			await insertIntoDatabase(
+				scrapedCharacterData,
+				"characters",
+				"characters"
+			);
+		}
+	} catch (error) {
+		console.error("Error scraping characters in main:", error);
+	}
+
+	try {
+		const scrapedSupportData = await scrapeGameTora(
+			"supports",
+			".sc-73e3e686-1.iAslZY"
+		);
+		if (scrapedSupportData) {
+			await insertIntoDatabase(
+				scrapedSupportData,
+				"supports",
+				"supports"
+			);
+		}
+	} catch (error) {
+		console.error("Error scraping support cards in main:", error);
+	}
+}
+
+main();
